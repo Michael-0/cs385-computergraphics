2a0d6ac366288d67865c901c535c44fa34ec144b
Michael-0 <54590632+Michael-0@users.noreply.github.com>
2022-02-28 19:28:24 -0800

    Completed Assignment 3
    figured out the viewing and projection transformations

--- assignment3/Cube.js
+++ assignment3/Cube.js
@@ -1,8 +1,7 @@
-const DefaultNumSides = 8;
 
-function Cube( gl, numSides, vertexShaderId, fragmentShaderId ) {
+function Cube( gl, vertexShaderId, fragmentShaderId ) {
 
-    // Initialize the shader pipeline for this object using either shader ids
+	// Initialize the shader pipeline for this object using either shader ids
     //   declared in the application's HTML header, or use the default names.
     //
     var vertShdr = vertexShaderId || "Cube-vertex-shader";
@@ -17,11 +16,9 @@ function Cube( gl, numSides, vertexShaderId, fragmentShaderId ) {
         return;
     }
 
-    var n = numSides || DefaultNumSides; // Number of sides
-
-    // Initialize temporary arrays for the Cube's indices and vertex positions
-    //
-    var positions = [
+	// Initialize arrays for the Cube's indices and vertex positions
+	//
+	var positions = [
 		0,1,0, // vertex 0
 		0,0,0, // vertex 1
 		1,0,0, // vertex 2
@@ -31,15 +28,20 @@ function Cube( gl, numSides, vertexShaderId, fragmentShaderId ) {
 		0,1,1, // vertex 6
 		0,0,1  // vertex 7
 	 ];
-	// 8 unique vertices from 0 - 7
+
 	var indices = [
 		0,1,3, 2, // back face on x,y
 		4, 5,	  // right face on y,z
 		6, 7,	  // front face on x,y
-		0, 1	  // left face on y,z
+		0, 1,	  // left face on y,z
+		0,3,6, 4, // top
+		1,7,2, 5 // bottom
 	];
-	console.log(indices);
 
+
+    this.positions = { numComponents : 3 };
+
+    this.indices = { count : indices.length };
 
     this.positions.buffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, this.positions.buffer );
@@ -52,8 +54,26 @@ function Cube( gl, numSides, vertexShaderId, fragmentShaderId ) {
     this.positions.attributeLoc = gl.getAttribLocation( this.program, "aPosition" );
     gl.enableVertexAttribArray( this.positions.attributeLoc );
 
+	// uniform plumbing for the transformations
+	this.Rloc = gl.getUniformLocation(this.program, "R");
+	this.R = mat4();
+
+	this.Lloc = gl.getUniformLocation(this.program, "T");
+	this.T = mat4();
+
+	this.perspProjloc = gl.getUniformLocation(this.program, "perspProj");
+	this.perspProj = mat4();
+
+	this.viewTransloc = gl.getUniformLocation(this.program, "viewTrans");
+	this.viewTrans = mat4();
+
     this.render = function () {
         gl.useProgram( this.program );
+
+		gl.uniformMatrix4fv(this.Rloc, gl.FALSE, flatten(this.R));
+		gl.uniformMatrix4fv(this.Lloc, gl.FALSE, flatten(this.T));
+		gl.uniformMatrix4fv(this.perspProjloc, gl.FALSE, flatten(this.perspProj));
+		gl.uniformMatrix4fv(this.viewTransloc, gl.FALSE, flatten(this.viewTrans));
 
         gl.bindBuffer( gl.ARRAY_BUFFER, this.positions.buffer );
         gl.vertexAttribPointer( this.positions.attributeLoc, this.positions.numComponents,
@@ -61,13 +81,21 @@ function Cube( gl, numSides, vertexShaderId, fragmentShaderId ) {
 
         gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, this.indices.buffer );
 
-        // Draw the cube's base
-        //
-        gl.drawElements( gl.POINTS, this.indices.count, gl.UNSIGNED_SHORT, 0 );
+		// Drawing 4 sides of the cube using triangle strip only
+		// reading the first 10 elements
+        gl.drawElements( gl.TRIANGLE_STRIP, 10, gl.UNSIGNED_SHORT, 0 );
 
-        // Draw the cube's top
-        //
-        var offset = this.indices.count * 2 /* sizeof(UNSIGNED_INT) */;
-        gl.drawElements( gl.POINTS, this.indices.count, gl.UNSIGNED_SHORT, offset );
+		// Drawing top of cube with triangle strip in the same indeces buffer
+		// but only reading the next 4 points after the side finished. 10 * 2
+		// because it is the offset in bytes. Each element is 2 bytes long, and
+		// there are 10 indices before getting to the top part
+		gl.drawElements( gl.TRIANGLE_STRIP, 4, gl.UNSIGNED_SHORT, 10*2);
+
+		// Drawing bottom of cube with triangle strip in the same indeces buffer
+		// but only reading the next 4 points after the top finished. 10 * 2
+		// because it is the offset in bytes. Each element is 2 bytes long, and
+		// there are 14 indices before getting to the bottom part
+		gl.drawElements( gl.TRIANGLE_STRIP, 4, gl.UNSIGNED_SHORT, 14*2);
+
     }
 };
