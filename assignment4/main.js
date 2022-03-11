58a732841a45651404431b8628cf49629b275b1d
Michael-0 <54590632+Michael-0@users.noreply.github.com>
2022-03-10 17:46:47 -0800

    redownloaded initial files

--- assignment4/main.js
+++ assignment4/main.js
@@ -1,6 +1,4 @@
 
-"use strict";
-
 var gl;
 
 function init() {
@@ -9,7 +7,7 @@ function init() {
 
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.enable(gl.DEPTH_TEST);
-   
+
     // Add your sphere creation and configuration code here
 
     requestAnimationFrame(render);
@@ -20,7 +18,7 @@ function render() {
     // Update your motion variables here
 
     gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
-    
+
     // Add your rendering sequence here
 
     requestAnimationFrame(render);
