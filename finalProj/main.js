422a75caebbbf9e6556a59714a2f5ccdd677e2d2
Michael-0 <54590632+Michael-0@users.noreply.github.com>
2022-05-13 17:58:42 -0700

    trying to figure out bug: object resets position when trying to rotate

--- finalProj/main.js
+++ finalProj/main.js
@@ -1,24 +1,46 @@
 var width;
 var height;
 var cube;
-var rotation;
-var dx = 1;
-var dy = 1;
-var zoomAmount = 1;
+var rotation = mat4();
+var dx = 0;
+var dy = 0;
+var overallX = 1;
+var overallY = 1;
+var zoomAmount = 5;
 
 function render() {
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 	var near = 1;
-	var far = 15;
+	var far = 20;
 
+	if ((Math.abs(dx) > 0 || Math.abs(dy) > 0) && cube.R !== rotation)
+	{
+		//console.log("in if statement");
+		//cube.R = rotate(Math.sqrt(dx*dx + dy*dy), vec3(-dx, dy, dx-dy));
+		rotation = cube.R;
+		cube.R = rotate(Math.sqrt(dx*dx + dy*dy), vec3(-dx, -dy, dx+dy));
+		// cube.R = rotate(Math.sqrt(overallX*overallX + overallY*overallY), vec3(-dx, -dy, dx+dy));
+	}
 	
-	cube.R = rotate(Math.sqrt(dx*dx + dy*dy), vec3(-dx, -dy, -dx+dy));
+	
+
+	// if (cube.hasOwnProperty("R"))
+	// {
+	// 	//console.log(cube.R);
+	// 	cube.R = add(rotate(Math.sqrt(dx*dx + dy*dy), vec3(-dx, -dy, dx+dy)), cube.R);
+	// }
+	// else
+	// {
+	// 	console.log("In else");
+	// 	//console.log(cube.R);
+	// 	cube.R = rotate(Math.sqrt(dx*dx + dy*dy), vec3(-dx, -dy, -dx+dy));
+	// }
 
 	cube.perspProj = perspective(90, width / height, near, far); // 4 number arguments: fovy, aspect, near, far
 
-	// moving the eye just for fun to make the cube do more interesting dancing
-	let eyeVec = vec3(1, 1, zoomAmount);
-	let atVec = vec3(-1, 0, -1);
+	// moving the eye forward or back to zoom in or out
+	let eyeVec = vec3(0, 0, zoomAmount);
+	let atVec = vec3(0, 0, -1);
 	let upVec = vec3(1, 1, 1);
 	cube.viewTrans = lookAt(eyeVec, atVec, upVec); // 3 vec3 arguments: eye, at, up
 
@@ -33,6 +55,7 @@ function init() {
 	height = canvas.clientHeight;
 	gl.clearColor(.25, .5, .75, 1);
 	cube = new Cube(gl);
+	//cube.R = mat4(1);
 	gl.enable(gl.DEPTH_TEST);
 	gl.enable(gl.CULL_FACE);
 	gl.cullFace(gl.BACK);
@@ -46,6 +69,9 @@ function init() {
 		var y = event.clientY;
 		dx = x - startX;
 		dy = y - startY;
+		overallX += dx;
+		overallY += dy;
+		// rotation = Math.sqrt(dx*dx + dy*dy);
 		console.log(dx, dy);
 	}
 	canvas.onmousedown = function (event) {
@@ -55,6 +81,7 @@ function init() {
 	};
 	canvas.onmouseup = function (event) {
 		canvas.removeEventListener("mousemove", mousemove);
+		
 	};
 	canvas.addEventListener("wheel", zoom);
 	render();
